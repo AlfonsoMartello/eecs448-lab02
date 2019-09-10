74168ae1fb99d4a7f4ca628848e87ae012b21b52
Alfonso Martello <alfonsomartello5@gmail.com>
2019-09-09 21:36:07 -0500

    In LinkedList.hpp for member method search(T value), added code that searches through the linked list for the given value if the linked list is not empty. If the value is found, it sets bool isFound to true

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,17 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	if (!isEmpty())
+	{
+		while (temp->getNext() != nullptr)
+		{
+			if (temp->getValue() == value)
+			{
+				isFound = true;
+			}
+			temp = temp->getNext();
+		}
+	}
 
 	return(isFound);
 }
